760036f40bc634bfffd3ec3238123836424a547a
Nate Bridi <nate.bridi@gmail.com>
2026-03-19 13:58:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+This is a simple CSS demo demonstrating several new CSS features, including carousels, custom functions, attr(), and anchor positioning. 
---
+++ src/main.ts
@@ -0,0 +1,82 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+    <ul class="clocks">
+        <li class="clock-wrap" id="dark" data-name="Dark">
+            <div class="clock">
+                <div class="hour"></div>
+                <div class="minute"></div>
+                <div class="second"></div>
+                <span class="digit" data-digit="1"></span>
+                <span class="digit" data-digit="2"></span>
+                <span class="digit" data-digit="3"></span>
+                <span class="digit" data-digit="4"></span>
+                <span class="digit" data-digit="5"></span>
+                <span class="digit" data-digit="6"></span>
+                <span class="digit" data-digit="7"></span>
+                <span class="digit" data-digit="8"></span>
+                <span class="digit" data-digit="9"></span>
+                <span class="digit" data-digit="10"></span>
+                <span class="digit" data-digit="11"></span>
+                <span class="digit" data-digit="12"></span>
+            </div>
+        </li>
+        <li class="clock-wrap" id="light" data-name="Light">
+            <div class="clock style-2">
+                <div class="hour"></div>
+                <div class="minute"></div>
+                <div class="second"></div>
+                <span class="digit" data-digit="1"></span>
+                <span class="digit" data-digit="2"></span>
+                <span class="digit" data-digit="3"></span>
+                <span class="digit" data-digit="4"></span>
+                <span class="digit" data-digit="5"></span>
+                <span class="digit" data-digit="6"></span>
+                <span class="digit" data-digit="7"></span>
+                <span class="digit" data-digit="8"></span>
+                <span class="digit" data-digit="9"></span>
+                <span class="digit" data-digit="10"></span>
+                <span class="digit" data-digit="11"></span>
+                <span class="digit" data-digit="12"></span>
+            </div>
+        </li>
+        <li class="clock-wrap style-3" id="vibrant" data-name="Vibrant">
+            <div class="clock">
+                <div class="hour"></div>
+                <div class="minute"></div>
+                <div class="second"></div>
+            
+
+                <span class="digit" data-digit="3"></span>
+                
+                <span class="digit" data-digit="6"></span>
+                
+                <span class="digit" data-digit="9"></span>
+                
+                <span class="digit" data-digit="12"></span>
+            </div>
+        </li>
+    </ul>
+    <div class="markers">
+        <a href="#dark">Dark</a>
+        <a href="#light">Light</a>
+        <a href="#vibrant">Vibrant</a>
+    </div>`
+    ;
+
+const hourEl = app.querySelectorAll<HTMLDivElement>('.hour')!
+const minuteEl = app.querySelectorAll<HTMLDivElement>('.minute')!
+const secondEl = app.querySelectorAll<HTMLDivElement>('.second')!
+
+function tick() {
+  const now = new Date()
+  hourEl.forEach((elem) => {elem.dataset.progress = String(now.getHours())});
+  minuteEl.forEach((elem) => {elem.dataset.progress = String(now.getMinutes())});
+  secondEl.forEach((elem) => {elem.dataset.progress = String(now.getSeconds())});
+}
+
+tick()
+//setInterval(tick, 1000)
+
