4c924c4c8860fd21cec58098b2cd5a05d5b76593
Nate Bridi <nate.bridi@gmail.com>
2026-03-24 10:44:00 -0400

    style updates

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # README
 
-This is a simple CSS demo demonstrating several new CSS features, including carousels, custom functions, attr(), and anchor positioning. 
+This is a simple CSS demo demonstrating several new (as of early 2026) CSS features, including carousels, custom functions, attr(), and anchor positioning. All this without any of the weird hacks (like hidden input elements) that have enabled interactive functionality in the past, nor convoluted CSS. 
+
+Functioning clocks in several styles, navigable with an carousel, including pleasant animated tabs.
+
+The only Javascript on the page is to initiatlize the clock times, but this could be inserted with any backend script. No scripting is involved in any of the functionality on the page.
+
+
+## CSS features 
+- [CSS Carousels](https://developer.mozilla.org/en-US/docs/Web/CSS/Guides/Overflow/Carousels)
+- [Custom `@functions`](https://developer.mozilla.org/en-US/docs/Web/CSS/Guides/Custom_functions_and_mixins)
+- [`attr()` used outside of `content` property](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Values/attr)
+- [Named anchor elements](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/anchor-name)
--- src/main.ts
+++ src/main.ts
@@ -42,27 +42,22 @@ app.innerHTML = `
                 <span class="digit" data-digit="12"></span>
             </div>
         </li>
-        <li class="clock-wrap style-3" id="vibrant" data-name="Vibrant">
-            <div class="clock">
+        <li class="clock-wrap" id="vibrant" data-name="Vibrant">
+            <div class="clock style-3">
                 <div class="hour"></div>
                 <div class="minute"></div>
                 <div class="second"></div>
-            
-
                 <span class="digit" data-digit="3"></span>
-                
                 <span class="digit" data-digit="6"></span>
-                
                 <span class="digit" data-digit="9"></span>
-                
                 <span class="digit" data-digit="12"></span>
             </div>
         </li>
     </ul>
     <div class="markers">
-        <a href="#dark">Dark</a>
-        <a href="#light">Light</a>
-        <a href="#vibrant">Vibrant</a>
+        <a class="marker" href="#dark">Dark</a>
+        <a class="marker" href="#light">Light</a>
+        <a class="marker" href="#vibrant">Vibrant</a>
     </div>`
     ;
 
@@ -70,13 +65,8 @@ const hourEl = app.querySelectorAll<HTMLDivElement>('.hour')!
 const minuteEl = app.querySelectorAll<HTMLDivElement>('.minute')!
 const secondEl = app.querySelectorAll<HTMLDivElement>('.second')!
 
-function tick() {
-  const now = new Date()
-  hourEl.forEach((elem) => {elem.dataset.progress = String(now.getHours())});
-  minuteEl.forEach((elem) => {elem.dataset.progress = String(now.getMinutes())});
-  secondEl.forEach((elem) => {elem.dataset.progress = String(now.getSeconds())});
-}
-
-tick()
-//setInterval(tick, 1000)
+const now = new Date()
+hourEl.forEach((elem) => {elem.dataset.progress = String(now.getHours() + now.getMinutes()/60)});
+minuteEl.forEach((elem) => {elem.dataset.progress = String(now.getMinutes())});
+secondEl.forEach((elem) => {elem.dataset.progress = String(now.getSeconds())});
 
